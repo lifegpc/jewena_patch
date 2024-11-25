c7ab7ab4da63f237027b668fab2630e306cb8c13
lifegpc <root@lifegpc.com>
2024-11-25 19:56:00 +0800

    Fix default font

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,6 +1,10 @@
 #include <Windows.h>
 #include "detours.h"
 #include <stdio.h>
+#include "wchar_util.h"
+
+static HFONT(WINAPI *TrueCreateFontW)(int nHeight, int nWidth, int nEscapement, int nOrientation, int fnWeight, DWORD dwItalic, DWORD dwUnderline, DWORD dwStrikeOut, DWORD dwCharSet, DWORD dwOutPrecision, DWORD dwClipPrecision, DWORD dwQuality, DWORD dwPitchAndFamily, LPCWSTR lpFaceName) = CreateFontW;
+static HFONT(WINAPI *TrueCreateFontA)(int nHeight, int nWidth, int nEscapement, int nOrientation, int fnWeight, DWORD dwItalic, DWORD dwUnderline, DWORD dwStrikeOut, DWORD dwCharSet, DWORD dwOutPrecision, DWORD dwClipPrecision, DWORD dwQuality, DWORD dwPitchAndFamily, LPCSTR lpFaceName) = CreateFontA;
 
 char* to_utf8(char* target, const char* source, UINT cp) {
     int count = MultiByteToWideChar(cp, MB_ERR_INVALID_CHARS, source, -1, NULL, 0);
@@ -33,19 +37,50 @@ PVOID GetHandle() {
     return (char*)hModule + 0xf3c20;
 }
 
+static PVOID h = nullptr;
+
+HFONT WINAPI HookedCreateFontW(int nHeight, int nWidth, int nEscapement, int nOrientation, int fnWeight, DWORD dwItalic, DWORD dwUnderline, DWORD dwStrikeOut, DWORD dwCharSet, DWORD dwOutPrecision, DWORD dwClipPrecision, DWORD dwQuality, DWORD dwPitchAndFamily, LPCWSTR lpFaceName) {
+    std::wstring name(lpFaceName);
+    if (name == L"Meiryo") {
+        lpFaceName = L"微软雅黑";
+    }
+    return TrueCreateFontW(nHeight, nWidth, nEscapement, nOrientation, fnWeight, dwItalic, dwUnderline, dwStrikeOut, dwCharSet, dwOutPrecision, dwClipPrecision, dwQuality, dwPitchAndFamily, lpFaceName);
+}
+
+HFONT WINAPI HookedCreateFontA(int nHeight, int nWidth, int nEscapement, int nOrientation, int fnWeight, DWORD dwItalic, DWORD dwUnderline, DWORD dwStrikeOut, DWORD dwCharSet, DWORD dwOutPrecision, DWORD dwClipPrecision, DWORD dwQuality, DWORD dwPitchAndFamily, LPCSTR lpFaceName) {
+    UINT cp[] = { CP_UTF8, CP_OEMCP, CP_ACP, 932 };
+    std::wstring font;
+    for (int i = 0; i < 4; i++) {
+        if (wchar_util::str_to_wstr(font, lpFaceName, cp[i])) {
+            if (font == L"Meiryo") {
+                font = L"微软雅黑";
+            }
+            return TrueCreateFontW(nHeight, nWidth, nEscapement, nOrientation, fnWeight, dwItalic, dwUnderline, dwStrikeOut, dwCharSet, dwOutPrecision, dwClipPrecision, dwQuality, dwPitchAndFamily, font.c_str());
+        }
+    }
+    if (!strcmp(lpFaceName, "Meiryo")) {
+        lpFaceName = "Microsoft YaHei";
+    }
+    return TrueCreateFontA(nHeight, nWidth, nEscapement, nOrientation, fnWeight, dwItalic, dwUnderline, dwStrikeOut, dwCharSet, dwOutPrecision, dwClipPrecision, dwQuality, dwPitchAndFamily, lpFaceName);
+}
+
 extern "C" __declspec(dllexport) void Attach() {
     DetourTransactionBegin();
     DetourUpdateThread(GetCurrentThread());
-    PVOID h = GetHandle();
+    h = GetHandle();
     DetourAttach(&h, (PVOID)jis_to_utf8);
+    DetourAttach(&TrueCreateFontW, HookedCreateFontW);
+    DetourAttach(&TrueCreateFontA, HookedCreateFontA);
     DetourTransactionCommit();
 }
 
 extern "C" __declspec(dllexport) void Detach() {
+    if (!h) return;
     DetourTransactionBegin();
     DetourUpdateThread(GetCurrentThread());
-    PVOID h = GetHandle();
     DetourDetach(&h, (PVOID)jis_to_utf8);
+    DetourDetach(&TrueCreateFontW, HookedCreateFontW);
+    DetourDetach(&TrueCreateFontA, HookedCreateFontA);
     DetourTransactionCommit();
 }
 
